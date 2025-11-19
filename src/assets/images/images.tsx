93294b76c4fd16cb23f35824311907c5d135ef59
root <root@DESKTOP-ST1N10K.localdomain>
2025-11-19 16:20:09 +0900

    feat: images세팅 svg이미지 파일 하나에서 통합 관리 가능하게 세팅

---
+++ src/assets/images/images.tsx
@@ -0,0 +1,45 @@
+import { forwardRef, type SVGProps } from 'react';
+
+import xIcon from './x_icon.svg?react'
+
+const ICONS = {
+  XIcon: xIcon,
+} as const;
+
+type Props = {
+  name: IconName;
+  size?: number | string;
+  decorative?: boolean; 
+  title?: string;      
+  customStyle?: React.CSSProperties;
+  fill? : string;
+  color?: string;
+} & SVGProps<SVGSVGElement>;
+
+export type IconName = keyof typeof ICONS;
+
+export const AppImage = forwardRef<SVGSVGElement, Props>(function AppIcon(
+  { name, decorative = false, title, customStyle, fill, color,...rest },
+  ref
+) {
+  const Svg = ICONS[name];
+  const aria = decorative
+    ? { role: 'img', 'aria-hidden': true }
+    : { role: 'img', 'aria-label': title ?? name };
+
+  const { ...restProps } = rest
+  const defaultStyle: React.CSSProperties = {
+    color: color ||"currentColor",
+    flexShrink:0
+  };
+
+  return (
+    <Svg
+      ref={ref}
+      {...aria}
+      {...restProps}
+      fill={fill ?? 'none'}
+      style={{ ...defaultStyle,...customStyle }}
+    />
+  );
+});
